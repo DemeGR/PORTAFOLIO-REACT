0ad0caa0242eb6a486dfebabfc829058fd9532cf
DemeGR <gurd970714@gs.utm.mx>
2022-08-25 12:02:12 -0500

    Updates

---
+++ precache-manifest.8f8c335a0a1cf36cb126e5449503b165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a944c15416747cd196ad",
+    "url": "/PORTAFOLIO-REACT/static/css/main.6857fe86.chunk.css"
+  },
+  {
+    "revision": "a944c15416747cd196ad",
+    "url": "/PORTAFOLIO-REACT/static/js/main.a944c154.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/PORTAFOLIO-REACT/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "be8a45cbac531f59aab6",
+    "url": "/PORTAFOLIO-REACT/static/js/runtime~main.be8a45cb.js"
+  },
+  {
+    "revision": "da27cef21c7154942a6cdf9d95f4a6d0",
+    "url": "/PORTAFOLIO-REACT/index.html"
+  }
+];
